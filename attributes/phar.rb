ff5b405ee7b2bdf1f83a16a2c8041b0914a757c1
Trent Petersen <trentpetersen.523@gmail.com>
2015-04-23 15:08:33 -0500

    Bump Phar_url to latest released version

--- attributes/phar.rb
+++ attributes/phar.rb
@@ -5,5 +5,5 @@
 # Copyright 2013-2014, Escape Studios
 #
 
-default['phpcb']['phar_url'] = 'https://github.com/Mayflower/PHP_CodeBrowser/releases/download/1.1.0/phpcb-1.1.0.phar'
+default['phpcb']['phar_url'] = 'https://github.com/Mayflower/PHP_CodeBrowser/releases/download/1.1.1/phpcb-1.1.1.phar'
 default['phpcb']['install_dir'] = '/usr/bin'
